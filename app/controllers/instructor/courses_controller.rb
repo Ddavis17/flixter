9ef73439103326ff50f77818dbb17e483b1e125f
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-07 15:22:07 +0000

    set up routing pages

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,8 +6,16 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def create 
-    @courses = current_user.courses.create(course_params)
-    redirect_to instructor_course_path(@course)
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+     redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+   end
+  end
+
+  def show
+    @course = Course.find(params[:id])
   end
 
   private
