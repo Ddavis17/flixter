bf7d62394997acb91acc6544ddbddca58528306f
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-11 03:59:58 +0000

    set up paymnt

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :before_authorized_for_current_lesson.section.course, only: [:show]
+
   def show
+
   end
 
   private
+
+  def before_authorized_for_current_lesson
+    if current_lesson.user != current_user
+      redirect_to course_path, alert: 'Enroll In This Course Before Watching Lessons'
+      render plain: 'unauthorized', status: :unauthorized
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
