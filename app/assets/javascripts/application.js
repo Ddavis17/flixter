13ac96b10d262928942394f41fe1ad332061d7ae
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-11 18:45:11 +0000

    added JQ2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,10 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
 //= require jquery-ui
+//= require jquery
+
