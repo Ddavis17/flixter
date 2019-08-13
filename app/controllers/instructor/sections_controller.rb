58f867b8dfc8180f40206af05c692a0f5b9e5589
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-13 01:06:37 +0000

    styled course page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :require_authorized_for_current_section, only: [:new, :create]
+  
 
   def new 
     @section = Section.new
@@ -10,8 +11,23 @@ class Instructor::SectionsController < ApplicationController
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end 
+
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
+  end
   
   private
+
+   def require_authorized_for_current_section
+    if current_lesson.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+   def current_section
+    @current_section ||= Section.find(params[:id])
+  end
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -22,12 +38,15 @@ class Instructor::SectionsController < ApplicationController
   
 
   helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
+    def current_course
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   namespace :instructor do
      resources :lessons, only: [:update]
      resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+     resources :lessons, only: [:new, :create]
     end
-    resources :sections, only: []
+    resources :sections, only: [:update]
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
