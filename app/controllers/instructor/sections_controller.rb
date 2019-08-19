8bad8ae065f6800e0b989fc20fa55135ce8ab436
Dustin Davis <dustin.davis2244@gmail.com>
2019-08-19 17:08:22 +0000

    --

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -3,7 +3,6 @@ class Instructor::SectionsController < ApplicationController
   before_action :require_authorized_for_current_course, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
 
-
   def create 
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
@@ -20,13 +19,13 @@ class Instructor::SectionsController < ApplicationController
   
   private
 
-   def require_authorized_for_current_section
+  def require_authorized_for_current_section
     if current_lesson.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
-   def current_section
+  def current_section
     @current_section ||= Section.find(params[:id])
   end
 
@@ -34,18 +33,16 @@ class Instructor::SectionsController < ApplicationController
     if current_course.user != current_user
       render plain: 'unauthorized', status: :unauthorized
     end
-  end
-    
-  
+  end  
 
   helper_method :current_course
-    def current_course
+  def current_course
+    if params[:course_id]
       @current_course ||= Course.find(params[:course_id])
     else
       current_section.course
     end
   end
-
 
   def section_params
     params.require(:section).permit(:title, :row_order_position)
--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,12 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
      resources :lessons, only: [:update]
-    resources :sections, only: [:update] do
+     resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :sections, only: [:create]
     resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end
 end
